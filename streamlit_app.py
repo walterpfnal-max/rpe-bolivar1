7ae174a71361938e9210db028c587f6d09d60c31
walterpfnal-max <walterpfnal@gmail.com>
2026-06-18 17:54:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-from gspread_dataframe import set_with_dataframe
+
 import gspread
 from google.oauth2.service_account import Credentials
 
